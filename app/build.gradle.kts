2b00c08b2fcd4a4ea1737298573165b40262fff7
luoshui <luoshuijs@outlook.com>
2026-03-30 20:17:59 +0800

    :wrench: Add release signing configuration with keystore properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,18 @@
-import com.android.build.api.dsl.Packaging
+import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     alias(libs.plugins.android.application)
+}
+
+fun getKeystoreProperties(): Properties? {
+    val keystorePropertiesFile = rootProject.file("keystore.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+        return keystoreProperties;
+    }
+    return null
 }
 
 android {
@@ -22,8 +33,21 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        val keystoreProperties = getKeystoreProperties();
+        if (keystoreProperties != null) {
+            create("release") {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
