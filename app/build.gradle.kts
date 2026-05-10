3d609506b484c0965090d2950813fe37553b093b
luoshui <luoshuijs@outlook.com>
2026-05-10 13:06:29 +0800

    :bookmark: Release 1.0.0 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         applicationId = "com.luoshui.paycardeditor"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0-rc2"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
